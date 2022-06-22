024fbde80c2a2c3bb7ae797924098d15e3fdd10c
konradi78 <102959617+konradi78@users.noreply.github.com>
2022-06-22 22:53:20 +0200

    Added Ilcarro Findimg

---
+++ src/test/java/com/telran/qa/FindImgIlcarro.java
@@ -0,0 +1,46 @@
+package com.telran.qa;
+
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.testng.annotations.AfterMethod;
+        import org.testng.annotations.BeforeMethod;
+        import org.testng.annotations.Test;
+
+        import java.util.List;
+        import java.util.concurrent.TimeUnit;
+
+public class FindImgIlcarro {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://ilcarro-1578153671498.web.app");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void findElementsByTagTest(){
+        //find element by tag
+        WebElement element = driver.findElement(By.tagName("img"));
+        System.out.println(element.getText());
+        System.out.println("Site opened");
+        List<WebElement> elements = driver.findElements(By.tagName("img"));
+        System.out.println(elements.size());
+
+        WebElement element1 = driver.findElement(By.tagName("img"));
+        System.out.println(element1.getText());
+
+
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        driver.quit();
+
+    }
+}
